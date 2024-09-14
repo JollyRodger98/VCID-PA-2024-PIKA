6204fd41b4ebae71c36c8590896c98c40efeaa93
Tim Waldesbühl <twaldesbuehl@gmail.com>
2024-09-14 17:20:59 +0200

    Removed Asiistuff

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,4 @@
 import multiprocessing
-from asciistuff import Banner, Lolcat
 # import setup_script
 import subprocess
 bind = "0.0.0.0:8000"
@@ -9,8 +8,6 @@ errorlog = "-"
 
 
 def on_starting(server):
-    print(f"\n{Lolcat(Banner("PIKA App", font="standard"), spread=1)}")
-
     # Can't import and execute script because somehow the app started by gunicorn then can't find flask routes.
     subprocess.run(["python", "setup_script.py"])
 
